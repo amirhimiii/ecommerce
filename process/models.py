e7ae0286da76c077a0c62ea830ee9eca774d1ae0
amirhossein <amirhossein.rahimi494@gmail.com>
2022-08-24 09:00:17 +0430

    complete add to cart

--- process/models.py
+++ process/models.py
@@ -6,19 +6,21 @@ from products.models import Product
 
 
 
-
+User = get_user_model()
 
 class Cart(models.Model):
-    user = models.ForeignKey(get_user_model(),
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
+    STATUS_PAID = [
+        ('NP','Not-Paid'),
+        ('P','Paid')
+    ]
+    
+    
+    user = models.ForeignKey(User,
+        on_delete=models.CASCADE,
         verbose_name='کاربر سفارش دهنده',
         related_name='orders',
     )
-    order_no = models.IntegerField(unique=True, null=True, blank=True, verbose_name='شماره سفارش')
-    is_paid = models.BooleanField(default=False, verbose_name='پرداخت شده/ نشده')
-    status = models.CharField(max_length=200, null=True, blank=True, verbose_name='وضعیت سفارش')
+    status = models.CharField(choices= STATUS_PAID,max_length=2 , null=True, blank=True, verbose_name='وضعیت سفارش')
     ordered = models.BooleanField(default=False)
     date_paid = models.DateTimeField(null=True, blank=True, verbose_name='تاریخ و زمان ثبت سفارش')
 
@@ -29,13 +31,13 @@ class Cart(models.Model):
 
 
 class CartItem(models.Model):
-    order = models.ForeignKey(Cart, on_delete=models.CASCADE , related_name = 'carts')
+    order = models.ForeignKey(Cart, on_delete=models.CASCADE , related_name = 'carts',blank=True)
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name = 'output')
     quantity = models.PositiveSmallIntegerField(default=0)
-    slug = models.SlugField()
+    # slug = models.SlugField()
 
     def __str__(self):
-        return f' {self.product.title} - {self.order}'
+        return self.product.title
 
     def get_total_price_url(self):
         return self.product.price * self.quantity
--- process/views.py
+++ process/views.py
@@ -3,14 +3,24 @@ from .models import Cart, CartItem
 from products.models import Product
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+import random
+from django.utils import timezone
+from django.core.exceptions import ObjectDoesNotExist
+
+rand = random.randint(100_000,500_000)
 
 
 @login_required
 def add_to_cart(request,slug):
     product = get_object_or_404(Product, slug=slug)
-    order = Cart.objects.filter(user=request.user, is_paid=False).first()
-    cart_item_created, cart_item  = CartItem.objects.get_or_create(product=product , order=order)
-
+    # order,create = Cart.objects.get_or_create(user=request.user,status='NP', ordered=True, date_paid=timezone.now())
+    # order = Cart.objects.filter(user=request.user).first()
+    try:
+        order = Cart.objects.get(user=request.user, ordered=True)
+    except ObjectDoesNotExist:
+        order = Cart.objects.create(user=request.user, status='NP', ordered=True, date_paid=timezone.now())
+    
+    cart_item_created, cart_item  = CartItem.objects.get_or_create(product=product , order=order )
     if cart_item_created:
         cart_item_created.quantity += 1
         cart_item_created.save()
@@ -22,14 +32,13 @@ def add_to_cart(request,slug):
 
 @login_required
 def remove_product_from_cart(request,slug):
-    cart_item= get_object_or_404(CartItem, order__is_paid=False, order__user=request.user, product__slug=slug)
+    cart_item= get_object_or_404(CartItem, order__user=request.user, product__slug=slug)
     if cart_item.quantity > 1:
         cart_item.quantity -= 1
         cart_item.save()
         messages.warning(request,'1 quantity -')
     else:
         messages.warning(request,'delete from cart')
-        messages.success(request,'add to cart')
         cart_item.delete()
     return redirect('product-list')
 
